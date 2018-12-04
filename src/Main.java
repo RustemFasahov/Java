68f3e8a986e567b3b80473b5cfdb636be86f1a80
RustemFasahov <rustem080@yandex.ru>
2018-12-04 22:00:05 +0300

    Выполнил домашнее задание по мере возможности)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Выполнение задания №1
@@ -39,24 +41,43 @@ public class Main {
                 System.out.print(x==y || x == (array4[y].length - y - 1) ? "[X]" : "[ ]");
             }
         }
+        System.out.println();
         
         //Выполнение задания №5
-        int[] array5 = new int[20];
+        int[] array5 = new int[10];
         for (int i=0; i<array5.length; i++){
             array5[i]= (int) (Math.random()*100);
         }
-        int maxNumber = 0;
+        int maxNumber = array5[0];
+        int minNumber = array5[0];
         for (int i=0; i<array5.length; i++){
             if (array5[i]> maxNumber) maxNumber = array5[i];
-        }
-        int minNumber = maxNumber;
-        for (int i=0; i<array5.length; i++){
-            if (array5[i]<minNumber) minNumber = array5[i];
+            if (array5[i]< minNumber) minNumber = array5[i];
         }
         for (int i=0; i<array5.length; i++){
             System.out.print(array5[i] + " ");
         }
 
+        //Задания из презентации
         System.out.println("Минимальное число " + minNumber + " Максимальное число " + maxNumber);
+        System.out.println("Введите степень для числа 2");
+        Scanner scaner = new Scanner(System.in);
+        int degree = scaner.nextInt();
+        int numberOfDegree = 2;
+        for(int i = 1; i < degree; i++){
+            numberOfDegree*=2;
+        }
+        System.out.printf("Число 2 в степени %d, равняется %d", degree, numberOfDegree);
+
+        int number1 = 0;
+        int numberY = 6;
+        int numberX = 6;
+        for (int i = 0; i<numberY; i++){
+            System.out.println();
+            for (int j = 0; j < numberX; j++) {
+                number1++;
+                System.out.printf("%4d",number1);
+            }
+        }
     }
 }
