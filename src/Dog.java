b60b161ea8d71af4eed52a0d97616029f565fa4e
Rustem <rustem080@yandex.ru>
2018-12-21 22:55:21 +0300

    Удалены ненужные классы

--- src/Dog.java
+++
@@ -1,7 +0,0 @@
-public class Dog extends Animal {
-    public Dog(){
-        super.runDistance = (int)(Math.random()*400) + 200;
-        super.swimingDistance = (int)(Math.random()*10) + 5;
-        super.jumpHeight = (float) (Math.random() + 0.5f);
-    }
-}
