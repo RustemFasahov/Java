73b3743343b3fc827f6bbc3ade6fb53e58167699
Rustem <rustem080@yandex.ru>
2018-12-26 14:14:33 +0300

    Написан класс запускающий игру

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,5 @@
 public class Test{
     public static void main(String[]args){
-        Cat[] cats = new Cat[3];
-        cats[0] = new Cat("Barsik", 50);
-        cats[1] = new Cat("Murzik", 75);
-        cats[2] = new Cat("Tom", 100);
-        Plate plate1 = new Plate(150);
-        for (int i = 0; i < cats.length; i++) {
-            cats[i].eat(plate1);
-            cats[i].info();
-        }
-        plate1.fill(100);
-        cats[2].eat(plate1);
-        cats[2].info();
-        plate1.info();
+        Game game = new Game();
     }
 }
